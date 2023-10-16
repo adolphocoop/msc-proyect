5e36ef7a685d6d02edc9e1b5c0d806bef262d6f3
adolpho <adolphovt12@gmail.com>
2023-10-15 18:24:04 -0600

    commit del domingo

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
    origin:[ 'http://localhost:5173',
    'https://frontend-7oy4.onrender.com',
-   "https://apimaestria.onrender.com"
+   "https://apimaestria.onrender.com",
 ],
    
    credentials: true
